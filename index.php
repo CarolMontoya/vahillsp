3b7bc58441622e284d79afd9d3a61cf125c46b4f
Carsten Dreesbach <herb@earthling.net>
2009-07-23 19:50:56 -0400

    Intermediate checkin beginning new CSS design - before ripping it apart again to work on the menus

--- index.php
+++ index.php
@@ -3,52 +3,44 @@
 	$siteTitle = "Home";
 	include_once "header.inc"; 
 ?>
-          <div class="left">
-            <table width="80%"
-                   border="0">
-              <tr>
-                <td>
-                  <div class="posrel">
-                    <div>
-                      <img src="images/collage-main.jpg"
-                           width="608"
-                           height="436"
-                           border="0"
-                           alt="Main collage"
-                           usemap="#main-collage-map">
-                    </div>
-                    <div class="image-title">
-                      Virginia Hills Pool: New for 2009
-                    </div>
-                    <div class="collage-header">
-                      <ul class="collage-links">
-                        <li>
-                          <a href="AdultSocials.html">Adult
-                          Social July 18, 7pm-12am</a>
-                        </li>
-                        <li>
-                          <a href="swim_team.html">Swim Team
-                          News<br>
-                          &nbsp;</a>
-                        </li>
-                        <li>
-                          <a href="schedule.html">Wednesday
-                          evening Raft Nights</a>
-                        </li>
-                      </ul>
-                    </div>
-                    <div class="collage-blurb">
-                      Nestled in the heart of the Virginia Hills
-                      Community, the Virginia Hills Pool provides
-                      a unique environment surrounded by rolling
-                      hills, shaded green space, and the wooded
-                      backdrop of <a href=
-                      "http://www.fairfaxcounty.gov/parks/rec/leerec.htm">
-                      Lee District Park</a>.
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </table>
-          </div>
+<div class="rounded">
+  <div class="posrel">
+    <div>
+      <img src="images/collage-main.jpg"
+           width="608"
+           height="436"
+           border="0"
+           alt="Main collage"
+           usemap="#main-collage-map">
+    </div>
+    <div class="image-title">
+      Virginia Hills Pool: New for 2009
+    </div>
+    <div class="collage-header">
+      <ul class="collage-links">
+        <li>
+          <a href="AdultSocials.html">Adult
+          Social July 18, 7pm-12am</a>
+        </li>
+        <li>
+          <a href="swim_team.html">Swim Team
+          News</a>
+        </li>
+        <li>
+          <a href="schedule.html">Wednesday
+          evening Raft Nights</a>
+        </li>
+      </ul>
+    </div>
+    <div class="collage-blurb">
+      Nestled in the heart of the Virginia Hills
+      Community, the Virginia Hills Pool provides
+      a unique environment surrounded by rolling
+      hills, shaded green space, and the wooded
+      backdrop of <a href=
+      "http://www.fairfaxcounty.gov/parks/rec/leerec.htm">
+      Lee District Park</a>.
+    </div>
+  </div>
+</div>
 <?php include_once "footer.inc"; ?>
