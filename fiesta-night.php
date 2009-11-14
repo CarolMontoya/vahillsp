40fdb06cb91cf58de7cd90415f9d6f3a06ae25d2
Carsten R. Dreesbach <herb@earthling.net>
2009-11-14 03:42:27 -0500

    Adding missing fiesta night file

---
+++ fiesta-night.php
@@ -0,0 +1,36 @@
+<?php
+global $siteTitle;
+$siteTitle = "Fiesta Night";
+include_once "header.inc";
+?>
+<div class="rounded">
+  <h2>FIESTA NIGHT</h2>
+  <h3>ADULT SOCIAL</h3>
+  <table>
+    <tr>
+      <td style="text-align:center;">
+        <img src="images/fiesta.gif">
+      </td>
+      <td style="text-align:center;">
+        Saturday, September 5th 8:00pm - 11:00pm<br>
+
+        Margaritas, Music and Merriment<br>
+
+        (Soda and water will also be available)<br>
+      </td>
+    </tr>
+    <tr>
+      <td style="text-align:center;">
+        Bring your favorite Mexican dish to share!!!<br>
+
+        $10.00 payable at the door<br>
+
+        The pool will be available for swimming
+      </td>
+      <td style="text-align:center;">
+        <img src="images/margherita.gif">
+      </td>
+    </tr>
+  </table>
+</div>
+<?php include_once "footer.inc"; ?>
