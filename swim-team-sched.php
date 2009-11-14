9bea3e41b3ed24ba8d28bf5a7b2f4c0765a125f8
Carsten R. Dreesbach <herb@earthling.net>
2009-11-14 03:09:07 -0500

    Adding missing swim team schedule back in

---
+++ swim-team-sched.php
@@ -0,0 +1,207 @@
+<?php
+global $siteTitle;
+$siteTitle = "Swim Team Schedule 2009";
+include_once "header.inc";
+?>
+<div class="rounded">
+
+  <h2>Swim
+  Team Schedule</h2>
+  <p align="center"><img src="images/swteam1.jpg" width="365" height="274"></p>
+  <h3>2009 Tidal Wave Schedule<br>
+  Division 17</h3>
+  <p><strong>Download
+  <a href="2009Calendar.pdf">Pool Calendar</a> (pdf)</strong></p>
+    <table width="85%" border="0" align="center">
+      <tr>
+        <td colspan="3"><p><strong>Important
+        Dates TBD:</strong></p>              <ul>
+          <li>Kick-Off
+          Meeting 7 pm; Swimsuit Sale 4-7 pm</li>
+          <li>Team Photo at morning practice</li>
+        </ul>
+      </td>
+    </tr>
+  </table>
+  <h5>Meet
+  Schedule</h5>
+  <table width="77%" border="1" align="center">
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>*
+      Remember that warm-ups start before meet times!</td>
+    </tr>
+    <tr>
+      <td>Saturday</td>
+      <td>June
+      13</td>
+      <td>Time
+      Trials  Highland Park at Virginia Hills 9 am (HOME)</td>
+    </tr>
+    <tr>
+      <td>Monday</td>
+      <td>June 15</td>
+      <td>&quot;B&quot; Meet
+      at Stratford Landing 6 pm (AWAY)</td>
+    </tr>
+    <tr>
+      <td>Saturday</td>
+      <td>June 20</td>
+      <td>&quot;A&quot;
+      Meet Herndon at Virginia Hills 9 am  (HOME)</td>
+    </tr>
+    <tr>
+      <td>Monday</td>
+      <td>June
+      22</td>
+      <td>&quot;B&quot;
+      Meet Little Hunting Park at Virginia Hills 6 pm  (HOME)</td>
+    </tr>
+    <tr>
+      <td width="18%">Saturday</td>
+      <td width="18%">June
+      27</td>
+      <td width="82%">&quot;A&quot;
+      Meet at Somerset Old Creek 9 am (AWAY)</td>
+    </tr>
+    <tr>
+      <td>Monday</td>
+      <td>June
+      29</td>
+      <td>&quot;B&quot; Meet
+      at Highland Park 6 pm (AWAY)</td>
+    </tr>
+    <tr>
+      <td>Saturday</td>
+      <td>July 4</td>
+      <td>&quot;A&quot; Meet Bryn Mar-Edsall Park at Virginia Hills 9 am  (HOME)</td>
+    </tr>
+    <tr>
+      <td>Monday</td>
+      <td>July
+      6</td>
+      <td>&quot;B&quot; Meet
+      Lorton Station at Virginia Hills 6 pm           (HOME)</td>
+    </tr>
+    <tr>
+      <td>Wednesday</td>
+      <td>July
+      8</td>
+      <td>Division
+      Relay Carnival at Edsall Park 3 pm</td>
+    </tr>
+    <tr>
+      <td>Saturday</td>
+      <td>July
+      11</td>
+      <td>&quot;A&quot; Meet
+      at Pinewood 9 am (AWAY)</td>
+    </tr>
+    <tr>
+      <td>Monday</td>
+      <td>July
+      13</td>
+      <td>&quot;B&quot; Meet
+      at Hayfield  6 pm (AWAY)</td>
+    </tr>
+    <tr>
+      <td>Saturday</td>
+      <td>July
+      18</td>
+      <td>&quot;A&quot;
+        Meet at Broyhill Crest  9
+      am (AWAY)</td>
+    </tr>
+    <tr>
+      <td height="21">Monday</td>
+      <td>July
+      20</td>
+      <td>IM
+      Invitational 5 pm</td>
+    </tr>
+    <tr>
+      <td>Saturday</td>
+      <td>July
+      25</td>
+      <td>Divisionals
+        at Pinewood 8:30 am; <br>
+      Team Banquet at 6 pm</td>
+    </tr>
+    <tr>
+      <td colspan="3">Details
+        will be posted each week for upcoming events.&nbsp; Saturday &#8220;A&#8221; Meets
+        typically
+        run from 8:00 am (warm-ups) to 11:30 am.&nbsp; Monday &quot;B&quot; Meets typically
+        run
+        from
+        5:
+        00
+      pm (warm-ups) to 8:30 pm.</td>
+    </tr>
+  </table>
+  <h5><a name="PracticeSched"></a>Practice
+  Schedule</h5>
+  <table width="75%" border="1" align="center">
+    <tr>
+      <td width="33%"><div align="left"></div></td>
+      <td width="25%" valign="top"><div align="center">Ages
+        8<br>
+        and under
+      </div></td>
+      <td width="22%" valign="top"><div align="center">Ages 9-12</div></td>
+      <td width="20%" valign="top"><div align="center">Ages
+      13-18</div></td>
+    </tr>
+    <tr>
+      <td><div align="left"><strong>Through
+        June 13<br>
+      (until school ends)</strong></div></td>
+      <td colspan="3">&nbsp;</td>
+    </tr>
+    <tr>
+      <td><div align="left"><br>
+        Monday thru Friday<br>
+      Afternoon practice only</div></td>
+      <td><div align="center">4:30
+        - 5:15 pm<br>
+      </div></td>
+      <td><div align="center">5:15
+      - 6:00 pm</div></td>
+      <td><div align="center">6:00
+      - 7:00 pm</div></td>
+    </tr>
+    <tr>
+      <td colspan="4"><hr></td>
+    </tr>
+    <tr>
+      <td><div align="left"><strong>June
+      16 - July 25</strong></div></td>
+      <td colspan="3">&nbsp;</td>
+    </tr>
+    <tr>
+      <td><div align="left"><br>
+        Monday
+        through Friday <br>
+      Morning practice</div></td>
+      <td><div align="center">10:15
+      - 11:00 am</div></td>
+      <td><div align="center">9:15 - 10:00 am</div></td>
+      <td><div align="center">8:00
+      - 9:00 am</div></td>
+    </tr>
+    <tr>
+      <td><div align="left">Tuesday
+        and Thursday <br>
+      Afternoon practice</div></td>
+      <td colspan="2"><div align="center">5:00
+      - 5:45 pm</div></td>
+      <td><div align="center">6:00
+      - 7:00 pm</div></td>
+    </tr>
+  </table>
+  <p align="center"><img src="images/TW2006TeamPic.jpg" width="475" height="276"></p>
+  <p>&nbsp;</p>
+  <div align="center"><img src="images/teampicbanquet.jpg" width="514" height="290"></div>
+</div>
+<?php include_once "footer.inc"; ?>
