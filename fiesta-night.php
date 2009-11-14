185e82929083dbcf6601cc2e50cb93f6cb868bd1
Carsten R. Dreesbach <herb@earthling.net>
2009-11-14 03:57:37 -0500

    Adding missing images for fiesta night, checking in redirectos in .htaccess

--- fiesta-night.php
+++ fiesta-night.php
@@ -6,31 +6,29 @@ include_once "header.inc";
 <div class="rounded">
   <h2>FIESTA NIGHT</h2>
   <h3>ADULT SOCIAL</h3>
-  <table>
-    <tr>
-      <td style="text-align:center;">
-        <img src="images/fiesta.gif">
-      </td>
-      <td style="text-align:center;">
-        Saturday, September 5th 8:00pm - 11:00pm<br>
+  <div class="yui-g">
+    <div class="yui-u first">
+      <img src="images/fiesta.gif">
+    </div>
+    <div class="yui-u">
+      Saturday, September 5th 8:00pm - 11:00pm<br>
 
-        Margaritas, Music and Merriment<br>
+      Margaritas, Music and Merriment<br>
 
-        (Soda and water will also be available)<br>
-      </td>
-    </tr>
-    <tr>
-      <td style="text-align:center;">
-        Bring your favorite Mexican dish to share!!!<br>
+      (Soda and water will also be available)<br>
+    </div>
+  </div>
+  <div class="yui-g">
+    <div class="yui-u first">
+      Bring your favorite Mexican dish to share!!!<br>
 
-        $10.00 payable at the door<br>
+      $10.00 payable at the door<br>
 
-        The pool will be available for swimming
-      </td>
-      <td style="text-align:center;">
-        <img src="images/margherita.gif">
-      </td>
-    </tr>
-  </table>
+      The pool will be available for swimming
+    </div>
+    <div class="yui-u">
+      <img src="images/margherita.gif">
+    </div>
+  </div>
 </div>
 <?php include_once "footer.inc"; ?>
