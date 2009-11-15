afc4d6cd331795175b18f1d00606a8d8cb5cefb0
Carsten Dreesbach <herb@earthling.net>
2009-11-15 09:07:22 -0500

    Adding previously uncomitted files from laptop

---
+++ teen-night.php
@@ -0,0 +1,20 @@
+<?php
+  global $siteTitle;
+  $siteTitle = "Teen Night";
+  include_once "header.inc"; 
+?>
+<div class="rounded">
+<p>Come and hang out for another</p>
+<p>Teen Night</p>
+<p>at the VA Hills Pool!!</p>
+<p>Monday, July 27</p>
+<p>8 pm - 11 pm</p>
+<p>$3.00 per teen (pay at the gate that night)</p>
+<p>for members and guests.</P
+<p>Pizza, snacks, & soda!</p>
+<p>Music, Swimming,</p>
+<p>Water Polo</p>
+<p>Questions: Call or e-mail Cheryl Schless at (703) 721-0923 / <a href="mailto:schlessc@aol.com">schlessc@aol.com</a></p>
+<p>Next Teen Night will be Monday, August 10 from 8 pm - 11 pm.</p>
+</div>
+<?php include_once "footer.inc"; ?>
