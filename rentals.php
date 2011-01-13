76890b05f75e244c18f9d814b7ad158921fa3587
Carsten Dreesbach <herb@earthling.net>
2011-01-13 00:18:52 -0500

    Fixing stupid Word copy-paste single quote fail!

--- rentals.php
+++ rentals.php
@@ -15,8 +15,8 @@
         times. Arrangements must be made in advance by contacting the Club  <a 
         href="mailto:&#105;&#110;&#102;&#111;&#64;&#118;&#97;&#104;&#105;&#108;&#108;&#115;&#112;&#111;&#111;&#108;&#46;&#111;&#114;&#103;">here</a>. 
         Use of picnic tables, the patio lounge, and access to the pool and all 
-        other facilities are included. The fee charged is against the member’s 
-        guest passes; guests are marked off against the member’s passes. 
+        other facilities are included. The fee charged is against the member's 
+        guest passes; guests are marked off against the member's passes. 
         Members receive 15 guest passes to begin the season; additional passes 
         are available for purchase at $5.00 per pass or 10 passes for $35.00. 
         Guests must be accounted for whether swimming or not, including parents 
