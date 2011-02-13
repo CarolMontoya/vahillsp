bd769b361580671ef813d4e4ff6e56b3e8767c43
Carsten Dreesbach <herb@earthling.net>
2011-02-13 10:12:40 -0500

    Removing old non-differentiated payment coupons

--- members.php
+++ members.php
@@ -9,9 +9,11 @@
           </h2>
           <div class="yui-g">
             <div class="yui-u first">
+<!--
               <p>
                 You can download the membership payment coupons <a href="docs/2011 Member Payment Coupons.pdf">here</a>.
               </p>
+-->
               <p>
                 <a href="docs/2011 January Statement & Invoice Info Sheet.pdf">2011 January Statement & Invoice Info Sheet</a>
               </p>
