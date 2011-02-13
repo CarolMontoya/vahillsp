28268fff633a5267b68cae60b7e034552b600cd0
Carsten Dreesbach <herb@earthling.net>
2011-02-13 10:13:59 -0500

    Fixing formatting

--- members.php
+++ members.php
@@ -8,22 +8,15 @@
             Members page
           </h2>
           <div class="yui-g">
-            <div class="yui-u first">
-<!--
-              <p>
-                You can download the membership payment coupons <a href="docs/2011 Member Payment Coupons.pdf">here</a>.
-              </p>
--->
-              <p>
-                <a href="docs/2011 January Statement & Invoice Info Sheet.pdf">2011 January Statement & Invoice Info Sheet</a>
-              </p>
-              <p>
-                <a href="docs/2011 Member Payment Coupons - Family.pdf">2011 Member Payment Coupons - Family</a>
-              </p>
-              <p>
-                <a href="docs/2011 Member Payment Coupons - Single.pdf">2011 Member Payment Coupons - Single</a>
-              </p>
-            </div>
+            <p>
+              <a href="docs/2011 January Statement & Invoice Info Sheet.pdf">2011 January Statement & Invoice Info Sheet</a>
+            </p>
+            <p>
+              <a href="docs/2011 Member Payment Coupons - Family.pdf">2011 Member Payment Coupons - Family</a>
+            </p>
+            <p>
+              <a href="docs/2011 Member Payment Coupons - Single.pdf">2011 Member Payment Coupons - Single</a>
+            </p>
           </div>
 </div>
 <?php include_once "footer.inc"; ?>
