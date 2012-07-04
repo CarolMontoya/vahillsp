d41c964c11982f7eab08610f662309a2c63c5a92
Carsten Dreesbach <herb@earthling.net>
2012-07-04 09:00:45 -0400

    Trying to fix the disappearing facebook badge issue - see http://kisahberuang.com/2010/07/13/solving-the-missing-facebook-badge/

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
                    "2012-06-16" => "opens late at 1:00pm for swim team meet",
                    "2012-06-18" => "closes early at 5:00pm for swim team meet",
                    "2012-06-30" => "closed today due to phone outage - call (703) 329-2360 to see if service has been restored",
-                   // "2012-07-01" => "WILL NOT OPEN today due to issues with the pumps",
+                   "2012-07-01" => "WILL NOT OPEN today due to issues with the pumps",
                    "2012-07-02" => "is operating normally today - come on by to try and beat the heat",
                   );
     $today = time();
