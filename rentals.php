e72dfecf50b9fabca2bcd596d9fe10850dc5f2bf
Carsten Dreesbach <herb@earthling.net>
2013-05-16 21:37:56 -0400

    Updated 2013 calendar and party infos

--- rentals.php
+++ rentals.php
@@ -10,18 +10,16 @@
   <div class="yui-g">
     <div class="yui-u first">
       <p>
-        Members only may host birthday parties, team parties and other similar 
-        events, during regular pool hours or by special arrangements at other 
-        times. Arrangements must be made in advance by contacting the Club  <a 
-        href="mailto:&#112;&#97;&#114;&#116;&#121;&#64;&#118;&#97;&#104;&#105;&#108;&#108;&#115;&#112;&#111;&#111;&#108;&#46;&#111;&#114;&#103;">here</a>. 
-        Use of picnic tables, the patio lounge, and access to the pool and all 
-        other facilities are included. The fee charged is against the member's 
-        guest passes; guests are marked off against the member's passes. 
-        Members receive 15 guest passes to begin the season; additional passes 
-        are available for purchase at $5.00 per pass or 10 passes for $35.00. 
-        Guests must be accounted for whether swimming or not, including parents 
-        chaperoning children.  Alcohol, except at Club- organized events, is 
-        prohibited. 
+        Members only may host birthday parties, team parties and other similar
+        events, during regular pool hours or by special arrangements at other
+        times. Arrangements must be made in advance by submitting a Party
+        Request Form. Use of picnic tables, the patio lounge, and access to the
+        pool and all other facilities are included. There is no rental fee but
+        all guests must be accounted for whether swimming or not, including
+        parents chaperoning children. Guest passes are available for purchase at
+        $4.00 per pass or 10 passes for $35.00. Alcohol, except at Club-
+        organized events, is prohibited.  Please refer to the Party Request Form
+        for complete rules for hosting a party.
       </p>
     </div>
     <div class="yui-u">
@@ -41,10 +39,16 @@
           alt="">
     </div>
     <div class="yui-u">
-      For scheduling parties, please fill out <a href="docs/VA Hills Pool Party Request.pdf">this form</a>
-      and send it to the email address given in the form. Also, please remember
-      to send the <a href="docs/VA Hills Swim Club Party Guest Form.pdf">guest list</a>
-      to the same email address 2 days prior to the party.
+      <p>
+        For scheduling parties, please fill out <a href="docs/VA Hills Pool Party Request.pdf">the Party Request form</a> and send it to the email
+        address given in the form. Also, please remember to send the <a
+        href="docs/VA Hills Swim Club Party Guest Form.xlsx">guest list</a> to
+        the same email address 2 days prior to the party.
+      </p>
+      <p>
+        For additional questions regarding party rentals, please click <a 
+        href="mailto:&#112;&#97;&#114;&#116;&#121;&#64;&#118;&#97;&#104;&#105;&#108;&#108;&#115;&#112;&#111;&#111;&#108;&#46;&#111;&#114;&#103;">here</a>.        
+      </p>
     </div>
   </div>
 </div>
