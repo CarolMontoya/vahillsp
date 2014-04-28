c636ea3a34dbe64c18df786dca929ec732366228
Carsten Dreesbach <carsten.dreesbach@opower.com>
2014-04-28 10:18:43 -0400

    Updating deploy.sh for CloudFlare

--- resources/deploy.sh
+++ resources/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rsync -avz --delete-after --exclude .git --exclude resources --exclude error_log --exclude BadAttemptList.txt -e ssh . vahillsp@vahillspool.org:~/www
-rsync -avz --delete-after --exclude script-kiddie-config.php --exclude BadAttemptList.txt -e ssh resources/script-kiddie vahillsp@vahillspool.org:~/
+rsync -avz --delete-after --exclude .git --exclude resources --exclude error_log --exclude BadAttemptList.txt -e ssh . vahillsp@direct.vahillspool.org:~/www
+rsync -avz --delete-after --exclude script-kiddie-config.php --exclude BadAttemptList.txt -e ssh resources/script-kiddie vahillsp@direct.vahillspool.org:~/
